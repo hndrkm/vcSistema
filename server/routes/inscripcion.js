3049deaa01f36830af7e47b46e6b05179b2ee4f0
hndrkm <hndrkrms@gmail.com>
2020-01-12 22:35:31 -0400

    nombre en controllesr

--- server/routes/inscripcion.js
+++ server/routes/inscripcion.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getInscripciones, createInscripcion, getInscripcion, deleteInscripcion, updateInscripcion } = require('../controllers/inscripcion.controller');
+const { getInscripciones, createInscripcion, getInscripcion, deleteInscripcion, updateInscripcion } = require('../controllers/inscripcion.Controller');
 
 router.route('/')
     .get(getInscripciones)
