ec66866868e5b26a84432adfcd7dd9a4f835c9b8
hndrkm <hndrkrms@gmail.com>
2020-01-12 22:50:35 -0400

    primero

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const app = require('./app');
 require('./database');
 
 async function main() {
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('frontend/build'));
+      
+        app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
+        });
+      }
     await app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
 }
